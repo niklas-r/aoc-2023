7a87c6f94d8c2a4af44a5c50f648ac32ca40c1da
Niklas Kahn <niklas@neatminds.se>
2023-12-01 11:14:48 +0100

    Day 1, step 1

--- AoC2023/Day1/Program.cs
+++ AoC2023/Day1/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection;
 
-Console.WriteLine("Hello, World!");
+var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, "../../../input.txt");
+var input = File.ReadLines(path);
+
+var total = 0;
+
+foreach (var l in input)
+{
+    int? first = null;
+    int? last = null;
+
+    foreach (var c in l.Where(char.IsDigit))
+    {
+        last = c - '0';
+        first ??= last;
+    }
+
+    total += int.Parse(first + "" + last);
+}
+
+Console.WriteLine(total);
