0d85a8de8af101ad5a5cb9b08cc0ac6aada3a9d0
Niklas Kahn <niklas@neatminds.se>
2023-12-01 15:08:26 +0100

    Day 1, step 2

--- AoC2023/Day1/Program.cs
+++ AoC2023/Day1/Program.cs
@@ -4,16 +4,41 @@ var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Lo
 var input = File.ReadLines(path);
 
 var total = 0;
+var words = new[]
+{
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+};
 
 foreach (var l in input)
 {
     int? first = null;
     int? last = null;
 
-    foreach (var c in l.Where(char.IsDigit))
+    foreach (var (c, i) in l.Select((c, i) => (c, i)))
     {
-        last = c - '0';
-        first ??= last;
+        if (char.IsDigit(c))
+        {
+            last = c - '0';
+            first ??= last;
+            continue;
+        }
+
+        foreach (var (w, j) in words.Select((w, j) => (w, j)))
+        {
+            if (l[i..].StartsWith(w))
+            {
+                last = j + 1;
+                first ??= j + 1;
+            }
+        }
     }
 
     total += int.Parse(first + "" + last);
