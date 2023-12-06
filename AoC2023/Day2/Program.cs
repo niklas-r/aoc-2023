bb12687ff51c4e5b84c564d61676c1b4b6022ce7
Niklas Kahn <niklas@neatminds.se>
2023-12-06 12:59:15 +0100

    Day 2, step 2

--- AoC2023/Day2/Program.cs
+++ AoC2023/Day2/Program.cs
@@ -1,6 +1,6 @@
 ﻿var input = File.ReadLines("../../../input.txt");
 
-Func<List<string>, int> p1 = (rows) =>
+Func<List<string>, List<Game>> parseGame = (rows) =>
 {
     var games = new List<Game>();
 
@@ -28,13 +28,39 @@ Func<List<string>, int> p1 = (rows) =>
                     _ => throw new Exception("Unknown color")
                 };
             }
+
             sets.Add(set);
         }
 
         games.Add(new Game(gameNumber, sets.ToArray()));
     }
+
+    return games;
+};
+
+Func<List<string>, int> p1 = (rows) =>
+{
+    var games = parseGame(rows);
+
     var validGames = games.Where(x => x.Sets.All(y => y is { Red: <= 12, Green: <= 13, Blue: <= 14 })).ToList();
     return validGames.Sum(x => x.Number);
+};
+
+Func<List<string>, int> p2 = (rows) =>
+{
+    var games = parseGame(rows);
+
+    var validGames = games.Select(x =>
+        x with { Sets = new []
+        {
+            x.Sets.Aggregate(new GameSet(0, 0, 0),
+                (acc, set) => new GameSet(
+                    Red: int.Max(acc.Red, set.Red),
+                    Green: int.Max(acc.Green, set.Green),
+                    Blue: int.Max(acc.Blue, set.Blue)))
+        }
+    });
+    return validGames.Sum(x => x.Sets.Sum(y => y.Red * y.Green * y.Blue));
 };
 
 var inputList = input.ToList();
@@ -43,7 +69,8 @@ if (inputList.Count == 0)
     throw new Exception("No input");
 }
 
-Console.WriteLine(p1(inputList));
+Console.WriteLine("p1: " + p1(inputList));
+Console.WriteLine("p2: " + p2(inputList));
 
 internal record GameSet(int Red, int Green, int Blue);
 internal record Game(int Number, GameSet[] Sets);
