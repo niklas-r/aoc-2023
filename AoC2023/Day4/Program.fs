10657be1ece6b56437eb4a7278fb2a81be450abf
Niklas Kahn <niklas@neatminds.se>
2023-12-08 12:11:35 +0100

    Day 4, step 1

---
+++ AoC2023/Day4/Program.fs
@@ -0,0 +1,35 @@
+﻿open System.IO
+
+let input: string seq = File.ReadLines("../../../input.txt")
+
+let double n = n * 2
+
+let parseNumbers (index: int) (rows: string seq) =
+    rows
+    |> Seq.map (_.Split(": ").[1].Split("|").[index])
+    |> Seq.map (fun line -> line.Trim().Replace("  ", " ").Split(" ") |> Array.map int |> Array.toList)
+    |> Seq.toList
+
+let parseCards (rows: string seq) =
+    let winningNumbers = parseNumbers 0 rows
+    let myNumbers = parseNumbers 1 rows
+
+    List.zip winningNumbers myNumbers
+
+
+let p1 input =
+    let cards = parseCards input
+
+    cards
+        |> Seq.map (fun (winningNumbers, myNumbers) ->
+            let matches = Set.intersect (Set.ofList winningNumbers) (Set.ofList myNumbers)
+
+            match matches with
+            | m when Seq.isEmpty m -> 0
+            | _ ->
+                matches
+                |> Set.toList
+                |> List.fold (fun acc _ -> if acc = 0 then 1 else double acc) 0)
+        |> Seq.sum
+
+printfn "p1: %A" (p1 input)
