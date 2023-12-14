4e94c1e8d3ab13d1e60a0bc8674ed77a4cf5c538
Niklas Kahn <niklas@neatminds.se>
2023-12-14 11:43:52 +0100

    Day 6, part 2

--- AoC2023/Day6/Day6Impl.fs
+++ AoC2023/Day6/Day6Impl.fs
@@ -2,7 +2,7 @@ module Day6Impl
 
 open System.Text.RegularExpressions
 
-type RaceResult = { time: int; distance: int }
+type RaceResult = { time: int64; distance: int64 }
 
 type RaceAttempt =
     { resultToBeat: RaceResult
@@ -15,24 +15,46 @@ let parseRaceResults (input: string array) =
 
     Seq.zip times distances
     |> Seq.map (fun (time, distance) ->
-        { time = int time.Value
-          distance = int distance.Value })
+        { time = int64 time.Value
+          distance = int64 distance.Value })
     |> Seq.toArray
 
 let doRaceAttempts (raceResults: RaceResult array) =
     raceResults
     |> Array.map (fun raceResult ->
         let results =
-            [| 0 .. raceResult.time |]
+            [| 0L .. raceResult.time |]
             |> Array.map (fun chargeTime ->
                 let distance = chargeTime * (raceResult.time - chargeTime)
-                { time = chargeTime; distance = distance })
+
+                { time = chargeTime
+                  distance = distance })
 
         { resultToBeat = raceResult
           myResults = results })
 
 let p1 (input: string seq) =
     let raceResults = parseRaceResults (input |> Seq.toArray)
+    let raceAttempts = doRaceAttempts raceResults
+
+    let numberOfNewTrackRecords =
+        raceAttempts
+        |> Array.map (fun raceAttempt ->
+            raceAttempt.myResults
+            |> Array.filter (fun myResult -> myResult.distance > raceAttempt.resultToBeat.distance))
+        |> Array.fold (fun acc x -> acc * x.Length) 1
+
+    numberOfNewTrackRecords
+
+let p2 (input: string seq) =
+    let raceResults =
+        parseRaceResults (input |> Seq.toArray)
+        |> Array.fold
+            (fun acc x ->
+                [| { time = int64 ((string acc[0].time) + (string x.time))
+                     distance = int64 ((string acc[0].distance) + (string x.distance)) } |])
+            [| { time = 0; distance = 0 } |]
+
     let raceAttempts = doRaceAttempts raceResults
 
     let numberOfNewTrackRecords =
--- AoC2023/Day6/Program.fs
+++ AoC2023/Day6/Program.fs
@@ -10,5 +10,7 @@ let run part runner input expected =
     assert (runner input = expected)
 
 run "example 1" Day6Impl.p1 example 288
-run "example 1" Day6Impl.p1 input 1083852
 run "part 1" Day6Impl.p1 input 1083852
+
+run "example 2" Day6Impl.p2 example 71503
+run "part 2" Day6Impl.p2 input 23501589
